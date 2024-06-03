fdfa9ac3d720baec09f6c9f8f9cd03b35b273342
Dans182 <dgaiteiro@gmail.com>
2024-06-03 12:50:21 +0200

    Guardado de codigo de la sesion anterior

--- pages/timeline/index.js
+++ pages/timeline/index.js
@@ -1,29 +1,35 @@
 import Link from "next/link";
 
-export default function Timeline({userName}) {
+export default function Timeline({ userName }) {
   return (
     <>
       <h1>This is the timeline of {userName}</h1>
       <Link href="/">Go home</Link>
-      <style jsx>{`
-      h1 {
-        font-size: 36px;
-        color: red;
-      }
-      `}
+      <style jsx>
+        {`
+          h1 {
+            font-size: 36px;
+            color: red;
+          }
+        `}
       </style>
     </>
   );
 }
 
-Timeline.getInitialProps = () => {
-    return fetch("/api/hello")
-    .then(res => res.json())
-    .then(response => {
-        console.log(response)
-        const {userName} = response
-        return {userName}
-    })
-}
+Timeline.getInitialProps = () => { //es importante destacar que el getInitialProps solo funciona en las pages. No funciona en componentes.
+    return fetch("http://localhost:3000/api/hello")
+      .then((res) => res.json())
+    
+  };
+
+// Timeline.getInitialProps = () => {
+//   return fetch("/api/hello")
+//     .then((res) => res.json())
+//     .then((response) => {
+//       const { userName } = response;
+//       return { userName };
+//     });
+// };
 //aca le estamos pasando las props que va a recibir el componente
-//Este metodo se esta ejecutando en el servidor Y lo que hace es ejecutarlo antes de renderizarlo. 
+//Este metodo se esta ejecutando en el servidor Y lo que hace es ejecutarlo antes de renderizarlo.
